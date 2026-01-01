c1abb089cd959b5906a9024166996ea5697b2a94
ran702 <ajeetranjeet495@gmail.com>
2026-01-01 21:06:29 +0530

    Add urls and learn somethings

--- backend/expense/views.py
+++ backend/expense/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import*
 
-# Create your views here.
+#signup API 
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':   # also use "delete" read="get"
+        data = json.loads(request.body)     #json to dictionary[frontend to python]
+        fullname = data.get('FullName')
+        email = data.get('Email')
+        password = data.get('Password')
+        
+        if UserDetail.objects.filter(Email=email).exists(): #filter=data read
+            return JsonResponse({"message":"email already exist"},status=400)
+        UserDetail.objects.create(FullName=fullname,Email=email,Password=password)
+        return JsonResponse({"message":"User registered Successfully"},status=201)
+        
+        
