56b990fd399b9fc284d46a3d3ebed245cf418152
ran702 <ajeetranjeet495@gmail.com>
2026-01-04 00:08:07 +0530

    Some change

--- backend/expense/models.py
+++ backend/expense/models.py
@@ -8,6 +8,9 @@ class UserDetail(models.Model):
     Password = models.CharField(max_length=50)
     RegDate = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return self.FullName
+    
 class Expense(models.Model):
     UserId = models.ForeignKey(UserDetail,on_delete=models.CASCADE)
     ExpenseDate = models.DateField(null=True,blank=True)
@@ -15,3 +18,5 @@ class Expense(models.Model):
     ExpenseCost = models.CharField(max_length=100)
     NoteDate = models.DateTimeField(auto_now_add=True)
         
+    def __str__(self):
+        return f"{self.ExpenseItem} - {self.ExpenseCost}"    
